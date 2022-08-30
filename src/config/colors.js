ab6b7eec88cf5a41a256f115345b76266ca72713
Kshitij Sharma <kshtjsharma68@gmail.com>
2022-08-30 21:36:05 +0530

    login with session

---
+++ src/config/colors.js
@@ -0,0 +1,18 @@
+import colors from "tailwindcss/colors";
+
+const Colors = {
+    backgrounds : {
+        success: 'bg-lime-400',
+        info: 'bg-cyan-300',
+        warning: 'bg-orange-400',
+        error: 'bg-red-600'
+      },
+    font: {
+        uccess: 'bg-lime-400',
+        info: 'bg-cyan-300',
+        warning: 'bg-orange-400',
+        error: 'bg-red-600' 
+    }
+}
+
+  export default Colors;
