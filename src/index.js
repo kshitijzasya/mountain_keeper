3cf443bb934b7757e4837adc6f3f88537ea9bd86
Kshitij Sharma <kshtjsharma68@gmail.com>
2022-09-01 21:08:50 +0530

    background image

--- src/index.js
+++ src/index.js
@@ -7,24 +7,24 @@ import "assets/styles/tailwind.css";
 
 // layouts
 
-import Admin from "layouts/Admin.js";
+// import Admin from "layouts/Admin.js";
 import Auth from "layouts/Auth.js";
 
 // views without layouts
 
 import Landing from "views/Landing.js";
-import Profile from "views/Profile.js";
-import Index from "views/Index.js";
+// import Profile from "views/Profile.js";
+// import Index from "views/Index.js";
 
 ReactDOM.render(
   <BrowserRouter>
     <Switch>
       {/* add routes with layouts */}
-      <Route path="/admin" component={Admin} />
+      {/* <Route path="/admin" component={Admin} /> */}
       <Route path="/auth" component={Auth} />
       {/* add routes without layouts */}
-      <Route path="/landing" exact component={Index} />
-      <Route path="/profile" exact component={Profile} />
+      {/* <Route path="/landing" exact component={Index} />
+      <Route path="/profile" exact component={Profile} /> */}
       <Route path="/" exact component={Landing} />
       {/* add redirect for first page */}
       <Redirect from="*" to="/" />
--- src/views/Landing.js
+++ src/views/Landing.js
@@ -1,22 +1,23 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import Ships from "../assets/img/ships.jpg";
 
 // components
 
-import Navbar from "components/Navbars/AuthNavbar.js";
+// import Navbar from "components/Navbars/AuthNavbar.js";
 import Footer from "components/Footers/Footer.js";
 
 export default function Landing() {
   return (
     <>
-      <Navbar transparent />
+      {/* <Navbar transparent /> */}
       <main>
         <div className="relative pt-16 pb-32 flex content-center items-center justify-center min-h-screen-75">
           <div
             className="absolute top-0 w-full h-full bg-center bg-cover"
             style={{
               backgroundImage:
-                "url('https://images.unsplash.com/photo-1557804506-669a67965ba0?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1267&q=80')",
+                `url(${Ships})`,
             }}
           >
             <span
@@ -26,16 +27,11 @@ export default function Landing() {
           </div>
           <div className="container relative mx-auto">
             <div className="items-center flex flex-wrap">
-              <div className="w-full lg:w-6/12 px-4 ml-auto mr-auto text-center">
+              <div className="w-full lg:w-6/12 px-4 ml-auto mr-auto text-center h-40">
                 <div className="pr-12">
                   <h1 className="text-white font-semibold text-5xl">
-                    Your story starts with us.
+                    Welcome to <br/>OURA Bathwares
                   </h1>
-                  <p className="mt-4 text-lg text-blueGray-200">
-                    This is a simple example of a Landing Page you can build
-                    using Notus React. It features multiple CSS components based
-                    on the Tailwind CSS design system.
-                  </p>
                 </div>
               </div>
             </div>
